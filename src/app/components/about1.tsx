83f82bf832701c2cd86f4491baafe437081bdc44
munibaweb123 <munibavirtualuni@gmail.com>
2024-12-09 00:18:10 +0500

    link of product

--- src/app/components/about1.tsx
+++ src/app/components/about1.tsx
@@ -1,6 +1,6 @@
 const About1 = () =>{
     return(
-        <div className="md:w-[1440px] md:h-[277px] w-[390px] h-[385px] mx-auto block md:flex items-center justify-between">
+        <div className="md:w-[1440px] md:h-[277px] w-[390px] h-[385px] mx-auto block md:flex items-center justify-evenly">
 
             <h3 className="text-2xl md:text-4xl w-[342px] md:w-[704px] h-[100px] ">A brand built on the love of craftmanship,
             quality and outstanding customer service</h3>
